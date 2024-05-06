2ed9f35572284d832266a50c495fe68a95880cc1
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-05-05 23:18:28 -0600

    Moved modal to a component

--- src/Screens/StoriesCatalogScreen.jsx
+++ src/Screens/StoriesCatalogScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Image, ImageBackground, Modal, Platform, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Colors from '../Util/Colors';
 import Row from '../Components/Row';
 import Col from '../Components/Col';
@@ -7,8 +7,7 @@ import Catalog from '../Data/StoryCatalog';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faArrowDown19, faArrowDown91, faArrowDownAZ, faArrowDownZA, faArrowUpAZ, faFilter, faLanguage, faMusic, faSort, faVolumeHigh, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
 import Util from '../Util/Functions';
-import Detail from '../Data/StoryDetail';
-import { width } from '@fortawesome/free-solid-svg-icons/fa0';
+import StoryModal from '../Components/StoryModal';
 
 
 
@@ -30,27 +29,20 @@ const StoriesCatalogScreen = () => {
 
     useEffect(() => {
         if (sortedBy == 'def') {
-            console.log('DEF sort')
             setSortedCat(dummyCat)
         } else if (sortedBy == 'az') {
-            console.log('AZ sort')
             let tempArr = dummyCat;
-            console.log('TEMP: ', tempArr)
             tempArr.sort(sort_by('title', false, (a) => a.toLowerCase()))
-            console.log('Sorted:', tempArr)
             setSortedCat(tempArr)
         } else if (sortedBy == 'za') {
-            console.log('ZA sort')
             let tempArr = dummyCat;
             tempArr.sort(sort_by('title', true, (a) => a.toLowerCase()))
             setSortedCat(tempArr)
         } else if (sortedBy == '19') {
-            console.log('19 sort')
             let tempArr = dummyCat;
             tempArr.sort(sort_by('readingTime', false, parseInt))
             setSortedCat(tempArr)
         } else if (sortedBy == '91') {
-            console.log('91 sort')
             let tempArr = dummyCat;
             tempArr.sort(sort_by('readingTime', true, parseInt))
             setSortedCat(tempArr)
@@ -84,14 +76,13 @@ const StoriesCatalogScreen = () => {
     }
 
     const readStory = (story) => {
-        console.log('Story to read: ', story);
         setShowStoryModal(true);
         setSelectedStory(story.id)
     }
 
     const dismissStory = () => {
         setShowStoryModal(false);
-        setSelectedStory(null)
+        setSelectedStory(0)
     }
 
     return (
@@ -185,103 +176,13 @@ const StoriesCatalogScreen = () => {
     );
 };
 
-const StoryModal = ({ storyId, visible, onClose }) => {
 
-    const [story, setStory] = useState(null)
-    const [storyDetail, setStoryDetail] = useState(null);
-
-    useEffect(() => {
-        for (i in Catalog) {
-
-            if (storyId == Catalog[i].id) {
-                console.log(`Cat: ${Catalog[i].id}`)
-                setStory(Catalog[i]);
-            }
-        }
-
-        for (i in Detail) {
-
-            if (storyId == Detail[i].id) {
-                console.log(`Det: ${Detail[i].id}`)
-                setStoryDetail(Detail[i]);
-            }
-        }
-    }, [storyId])
-
-    return (
-
-        <Modal animationType='slide' visible={visible} presentationStyle='pageSheet' onDismiss={() => { }} style={styles.modal}>
-            <View style={styles.modal}>
-                <Row>
-                    <Col styles={{ width: '65%' }}>
-                        <Text style={styles.modalTitle}>
-                            {story.title}
-                        </Text>
-                    </Col>
-                    <Col styles={{ width: '25%' }}>
-                        <Pressable style={styles.button} onPress={onClose}>
-                            <Text style={styles.buttonText}>Close</Text>
-                        </Pressable>
-                    </Col>
-                </Row>
-                <Text style={styles.modalReadingTime}>
-                    Reading Time: {story.readingTime} min
-                </Text>
-                <Image
-                    style={styles.normalImage}
-                    source={{
-                        uri: story.image,
-                    }}
-                />
-                <ScrollView style={styles.storyView}>
-                    {storyDetail.story.map((paragraph) => (
-                        <Text style={styles.paragraph}>{paragraph}</Text>
-                    ))}
-                </ScrollView>
-
-
-            </View>
-        </Modal>
-
-    )
-};
 
 const styles = StyleSheet.create({
     view: {
         height: '100%',
         backgroundColor: Colors.background,
         padding: 5
-    },
-    modal: {
-        height: '100%',
-        backgroundColor: Colors.background,
-        padding: 20,
-        alignItems: 'center'
-    },
-    modalTitle: {
-        color: Colors.titlePrimary,
-        fontSize: 20,
-        textAlign: 'left',
-        fontWeight: 'bold',
-        fontFamily: 'Helvetica'
-    },
-    text: {
-        color: Colors.titlePrimary,
-        fontSize: 30,
-        textAlign: 'center',
-        fontWeight: 'bold'
-    },
-    subtitle: {
-        color: Colors.titleSecondary,
-        fontSize: 20,
-        textAlign: 'center',
-        fontWeight: 'bold'
-    },
-    row: {
-        flexDirection: 'row',
-        paddingTop: 15,
-        paddingHorizontal: 15,
-        alignItems: 'center'
     },
     button: {
         backgroundColor: Colors.cardBackground,
@@ -297,18 +198,24 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
 
     },
-    tinyImage: {
-        width: 100,
-        height: 100,
-        borderRadius: 10,
-        resizeMode: 'stretch'
+    text: {
+        color: Colors.titlePrimary,
+        fontSize: 30,
+        textAlign: 'center',
+        fontWeight: 'bold'
     },
-    normalImage: {
-        width: 250,
-        height: 250,
-        borderRadius: 10,
-        resizeMode: 'stretch'
+    subtitle: {
+        color: Colors.titleSecondary,
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold'
     },
+    configBar: {
+        borderWidth: 1,
+        borderTopWidth: 2,
+        marginBottom: 5
+    },
+
     bookCard: {
         flexDirection: 'row',
         margin: 5,
@@ -331,32 +238,12 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.background,
         borderRadius: 20
     },
-    configBar: {
-
+    tinyImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+        resizeMode: 'stretch'
     },
-    storyView: {
-        backgroundColor: Colors.cardBackground,
-        width: '100%',
-        padding: 10,
-        margin: 10,
-        borderRadius: 20,
-        borderWidth: 1,
-        borderColor: Colors.titleSecondary
-    },
-    paragraph: {
-        color: Colors.titlePrimary,
-        textAlign: 'justify',
-        paddingVertical: 5,
-        fontWeight: '900',
-        fontFamily: 'helvetica',
-        fontSize: 16
-    },
-    modalReadingTime: {
-
-        color: Colors.titleSecondary,
-    },
-
-
 });
 
 export default StoriesCatalogScreen;
