baed7d0520e5a48637569946a99eb13fdb078952
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-05-05 22:53:14 -0600

    Modal added

--- src/Screens/StoriesCatalogScreen.jsx
+++ src/Screens/StoriesCatalogScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Image, ImageBackground, Platform, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, Modal, Platform, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Colors from '../Util/Colors';
 import Row from '../Components/Row';
 import Col from '../Components/Col';
@@ -7,6 +7,8 @@ import Catalog from '../Data/StoryCatalog';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faArrowDown19, faArrowDown91, faArrowDownAZ, faArrowDownZA, faArrowUpAZ, faFilter, faLanguage, faMusic, faSort, faVolumeHigh, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
 import Util from '../Util/Functions';
+import Detail from '../Data/StoryDetail';
+import { width } from '@fortawesome/free-solid-svg-icons/fa0';
 
 
 
@@ -17,6 +19,9 @@ const StoriesCatalogScreen = () => {
     const [sortedBy, setSortedBy] = useState('def');
     const [sortedCat, setSortedCat] = useState([]);
     const [dummyCat, setDummyCat] = useState([]);
+
+    const [selectedStory, setSelectedStory] = useState(null);
+    const [showStoryModal, setShowStoryModal] = useState(false);
 
     useEffect(() => {
         Util.shuffle(Catalog);
@@ -72,11 +77,29 @@ const StoriesCatalogScreen = () => {
         }
     }
 
+    const surpriseStory = () => {
+        const id = Math.floor(Math.random() * Catalog.length - 1)
+        setShowStoryModal(true);
+        setSelectedStory(id)
+    }
+
+    const readStory = (story) => {
+        console.log('Story to read: ', story);
+        setShowStoryModal(true);
+        setSelectedStory(story.id)
+    }
+
+    const dismissStory = () => {
+        setShowStoryModal(false);
+        setSelectedStory(null)
+    }
+
     return (
         <View style={styles.view}>
+            <StoryModal visible={showStoryModal} storyId={selectedStory} onClose={dismissStory} />
             <Row>
                 <Col styles={{ width: '25%' }}>
-                    <Pressable style={styles.button}>
+                    <Pressable style={styles.button} onPress={surpriseStory}>
                         <Text style={styles.buttonText}>Surprise</Text>
                     </Pressable>
                 </Col>
@@ -136,7 +159,7 @@ const StoriesCatalogScreen = () => {
             }
             <ScrollView >
                 {sortedCat.length > 0 ? (sortedCat.slice(0, 6).map((story) => (
-                    <Pressable key={story.id} style={styles.bookCard}>
+                    <Pressable key={story.id} style={styles.bookCard} onPress={readStory.bind(null, story)}>
                         <Row>
                             <Image
                                 style={styles.tinyImage}
@@ -162,11 +185,85 @@ const StoriesCatalogScreen = () => {
     );
 };
 
+const StoryModal = ({ storyId, visible, onClose }) => {
+
+    const [story, setStory] = useState(null)
+    const [storyDetail, setStoryDetail] = useState(null);
+
+    useEffect(() => {
+        for (i in Catalog) {
+
+            if (storyId == Catalog[i].id) {
+                console.log(`Cat: ${Catalog[i].id}`)
+                setStory(Catalog[i]);
+            }
+        }
+
+        for (i in Detail) {
+
+            if (storyId == Detail[i].id) {
+                console.log(`Det: ${Detail[i].id}`)
+                setStoryDetail(Detail[i]);
+            }
+        }
+    }, [storyId])
+
+    return (
+
+        <Modal animationType='slide' visible={visible} presentationStyle='pageSheet' onDismiss={() => { }} style={styles.modal}>
+            <View style={styles.modal}>
+                <Row>
+                    <Col styles={{ width: '65%' }}>
+                        <Text style={styles.modalTitle}>
+                            {story.title}
+                        </Text>
+                    </Col>
+                    <Col styles={{ width: '25%' }}>
+                        <Pressable style={styles.button} onPress={onClose}>
+                            <Text style={styles.buttonText}>Close</Text>
+                        </Pressable>
+                    </Col>
+                </Row>
+                <Text style={styles.modalReadingTime}>
+                    Reading Time: {story.readingTime} min
+                </Text>
+                <Image
+                    style={styles.normalImage}
+                    source={{
+                        uri: story.image,
+                    }}
+                />
+                <ScrollView style={styles.storyView}>
+                    {storyDetail.story.map((paragraph) => (
+                        <Text style={styles.paragraph}>{paragraph}</Text>
+                    ))}
+                </ScrollView>
+
+
+            </View>
+        </Modal>
+
+    )
+};
+
 const styles = StyleSheet.create({
     view: {
         height: '100%',
         backgroundColor: Colors.background,
         padding: 5
+    },
+    modal: {
+        height: '100%',
+        backgroundColor: Colors.background,
+        padding: 20,
+        alignItems: 'center'
+    },
+    modalTitle: {
+        color: Colors.titlePrimary,
+        fontSize: 20,
+        textAlign: 'left',
+        fontWeight: 'bold',
+        fontFamily: 'Helvetica'
     },
     text: {
         color: Colors.titlePrimary,
@@ -206,6 +303,12 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         resizeMode: 'stretch'
     },
+    normalImage: {
+        width: 250,
+        height: 250,
+        borderRadius: 10,
+        resizeMode: 'stretch'
+    },
     bookCard: {
         flexDirection: 'row',
         margin: 5,
@@ -230,7 +333,29 @@ const styles = StyleSheet.create({
     },
     configBar: {
 
-    }
+    },
+    storyView: {
+        backgroundColor: Colors.cardBackground,
+        width: '100%',
+        padding: 10,
+        margin: 10,
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: Colors.titleSecondary
+    },
+    paragraph: {
+        color: Colors.titlePrimary,
+        textAlign: 'justify',
+        paddingVertical: 5,
+        fontWeight: '900',
+        fontFamily: 'helvetica',
+        fontSize: 16
+    },
+    modalReadingTime: {
+
+        color: Colors.titleSecondary,
+    },
+
 
 });
 
